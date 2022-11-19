0005393592baef0ae80f47ebcd92c8f63ad746da
utxo.one <github@utxo.one>
2022-11-19 15:58:21 -0500

    Return struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,18 @@ mod models;
 
 use std::error::Error;
 
-pub async fn get_mempool() -> Result<(), Box<dyn Error>> {
+use models::{Mempool, RecommendedFees};
+
+pub async fn get_mempool() -> Result<Mempool, Box<dyn Error>> {
     let http_response = reqwest::get("https://mempool.space/api/mempool").await?;
     let response = http_response.json::<models::Mempool>().await?;
-    println!("{:#?}", response);
-    Ok(())
+
+    Ok(response)
 }
 
-pub async fn get_recommended_fees() -> Result<(), Box<dyn Error>> {
+pub async fn get_recommended_fees() -> Result<RecommendedFees, Box<dyn Error>> {
     let http_response = reqwest::get("https://mempool.space/api/v1/fees/recommended").await?;
     let response = http_response.json::<models::RecommendedFees>().await?;
-    println!("{:#?}", response);
-    Ok(())
+    
+    Ok(response)
 }
